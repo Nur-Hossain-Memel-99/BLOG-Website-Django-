7696f157629e1e2fac90ce37660e4e88927338e2
Nur Hossain Hemel <90263811+Nur-Hossain-Memel-99@users.noreply.github.com>
2022-06-21 13:08:01 +0600

    update

---
+++ home/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render,HttpResponse
+from datetime import datetime
+from home.models import Contact
+from django.contrib import messages
+# Create your views here.
+def index(request):
+    context = {
+        "vab":"Ok Got It",
+        "vab1":"Ok Got It"
+    }
+    return render(request,'index.html',context)
+    #return HttpResponse("this is home page")
+
+
+def about(request):
+    return render(request,'about.html')
+def services(request):
+    return render(request,'services.html')
+def contact(request):
+    if request.method == "POST":
+        name= request.POST.get('name')
+        email=  request.POST.get('email')
+        phone=  request.POST.get('phone')
+        desc=  request.POST.get('desc')
+        contact= Contact(name=name,email=email,phone=phone,desc=desc,date=datetime.today())
+        contact.save()
+        messages.success(request, 'Submitted Successfully! ')
+    return render(request,'contact.html')
